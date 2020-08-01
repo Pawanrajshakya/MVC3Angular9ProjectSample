f98bd62c61040c741f91229e0c1eadc5664808d9
PAWAN R SHAKYA <prshakya07@gmail.com>
2020-08-01 10:27:45 -0400

    d

---
+++ WebApplication1/Filters/CustomAuthorizationFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Helpers
+{
+    public class CustomAuthorizationFilter : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User.Identity.Name;
+        }
+    }
+}
